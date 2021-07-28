66d15caaa239e04d51232bd3c8df44fb1c75ef92
Joel Ek <contact@joelek.se>
2021-07-28 22:41:03 +0200

    Update test for Glesys module.

--- source/lib/glesys/index.test.ts
+++ source/lib/glesys/index.test.ts
@@ -4,9 +4,36 @@ import * as glesys from "./";
 async function test(): Promise<void> {
 	let config = glesys.config.Config.as(JSON.parse(libfs.readFileSync("./private/config/glesys.json", "utf-8")));
 	let client = glesys.makeClient(config);
-	let response = await client.listDomains({});
-	let payload = await response.payload();
-	console.log(JSON.stringify(payload, null, "\t"));
+	let domains = await (await client.listDomains({})).payload();
+	let domain = domains.response.domains.find((domain) => domain.domainname === config.domainname);
+	if (domain == null) {
+		throw "Expected to find a matching domain id!";
+	}
+	console.log(JSON.stringify({ domain }, null, "\t"));
+	let created = await (await client.createDomainRecord({
+		payload: {
+			domainname: domain.domainname,
+			host: "_acme-challenge",
+			type: "TXT",
+			data: "TBD1"
+		}
+	})).payload();
+	console.log(JSON.stringify({ created }, null, "\t"));
+	let updated = await (await client.updateDomainRecord({
+		payload: {
+			recordid: created.response.record.recordid,
+			host: "_acme-challenge",
+			type: "TXT",
+			data: "TBD2"
+		}
+	})).payload();
+	console.log(JSON.stringify({ updated }, null, "\t"));
+	let deleted = await (await client.deleteDomainRecord({
+		payload: {
+			recordid: created.response.record.recordid
+		}
+	})).payload();
+	console.log(JSON.stringify({ deleted }, null, "\t"));
 }
 
 test().catch((error) => console.log(String(error)));
