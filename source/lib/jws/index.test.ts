f092e8c029000ee71ec9b9525949b0a6d557e532
Joel Ek <contact@joelek.se>
2021-08-20 07:34:52 +0200

    Clean up JWS module.

--- source/lib/jws/index.test.ts
+++ source/lib/jws/index.test.ts
@@ -1,5 +1,20 @@
-import * as index from "./index";
+import * as libcrypto from "crypto";
+import * as lib from "./";
 
 (async () => {
-
+	let { privateKey, publicKey } = libcrypto.generateKeyPairSync(`rsa`, {
+		modulusLength: 4096,
+		publicExponent: 65537,
+		publicKeyEncoding: {
+			type: `pkcs1`,
+			format: `pem`
+		},
+		privateKeyEncoding: {
+			type: `pkcs1`,
+			format: `pem`
+		}
+	});
+	let blob = await lib.sign(privateKey, { horse: "hej" }, "testfolk");
+	let verification = await lib.verify(blob, publicKey);
+	console.log(verification);
 })();
--- source/lib/jws/index.ts
+++ source/lib/jws/index.ts
@@ -1,26 +1,29 @@
 import * as libcrypto from "crypto";
 import * as encoding from "../encoding";
+import * as json from "../json";
 
-type Structure = {
+// TODO: Add support for other signature algorithms.
+
+async function encode(json: json.Any): Promise<string> {
+	return Promise.resolve(json)
+		.then(encoding.convertJSONToString)
+		.then(encoding.convertStringToUTF8Buffer)
+		.then(encoding.convertBufferToBase64URLString);
+};
+
+export type Blob = {
 	protected: string;
 	payload: string;
 	signature: string;
 };
 
-async function encode(json: any): Promise<string> {
-	return Promise.resolve(json)
-		.then(encoding.convertJSONToString)
-		.then(encoding.convertStringToUTF8Buffer)
-		.then(encoding.convertBufferToBase64URLString);
-}
-
-async function sign(private_key: Buffer, protected_json: any, payload_json?: any): Promise<Structure> {
+export async function sign(private_key: libcrypto.KeyLike, protected_json?: json.Object, payload_json?: json.Any): Promise<Blob> {
 	let protected_base64url = await encode({
 		...protected_json,
-		alg: "RS256"
+		alg: `RS256`
 	});
-	let payload_base64url = (payload_json != null ? await encode(payload_json) : "");
-	let signer = libcrypto.createSign("SHA256");
+	let payload_base64url = (payload_json != null ? await encode(payload_json) : ``);
+	let signer = libcrypto.createSign(`SHA256`);
 	signer.update(`${protected_base64url}.${payload_base64url}`);
 	let signature = signer.sign(private_key);
 	let signature_base64url = await encoding.convertBufferToBase64URLString(signature);
@@ -29,9 +32,11 @@ async function sign(private_key: Buffer, protected_json: any, payload_json?: any
 		payload: payload_base64url,
 		signature: signature_base64url
 	};
-}
+};
 
-export {
-	Structure,
-	sign
+export async function verify(blob: Blob, public_key: libcrypto.KeyLike): Promise<boolean> {
+	let verifier = libcrypto.createVerify(`SHA256`);
+	verifier.update(`${blob.protected}.${blob.payload}`);
+	let signature = await encoding.convertBase64URLStringToBuffer(blob.signature);
+	return verifier.verify(public_key, signature);
 };
