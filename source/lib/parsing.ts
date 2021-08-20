4959252c14f60b69d1a7f9b0682f6a3dd4a0a65a
Joel Ek <contact@joelek.se>
2021-08-20 10:13:45 +0200

    Add parsing module.

---
+++ source/lib/parsing.ts
@@ -0,0 +1,47 @@
+export class Parser {
+	private buffer: Buffer;
+	private offset: number;
+
+	constructor(buffer: Buffer, offset?: number) {
+		this.buffer = buffer;
+		this.offset = offset ?? 0;
+	}
+
+	eof(): boolean {
+		return this.offset >= this.buffer.length;
+	}
+
+	signed(length: number, endian?: "big" | "little"): number {
+		if (endian === "little") {
+			let value = this.buffer.readIntLE(this.offset, length);
+			this.offset += length;
+			return value;
+		} else {
+			let value = this.buffer.readIntBE(this.offset, length);
+			this.offset += length;
+			return value;
+		}
+	}
+
+	try<A>(supplier: (reader: Parser) => A): A {
+		let offset = this.offset;
+		try {
+			return supplier(this);
+		} catch (error) {
+			this.offset = offset;
+			throw error;
+		}
+	}
+
+	unsigned(length: number, endian?: "big" | "little"): number {
+		if (endian === "little") {
+			let value = this.buffer.readUIntLE(this.offset, length);
+			this.offset += length;
+			return value;
+		} else {
+			let value = this.buffer.readUIntBE(this.offset, length);
+			this.offset += length;
+			return value;
+		}
+	}
+};
