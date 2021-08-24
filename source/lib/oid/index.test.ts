0841de45e21841abd6d3c31105f060db35ae5f67
Joel Ek <contact@joelek.se>
2021-08-24 12:17:53 +0200

    Clean up OID tests.

--- source/lib/oid/index.test.ts
+++ source/lib/oid/index.test.ts
@@ -2,67 +2,80 @@ import * as oid from "./";
 import * as parsing from "../parsing";
 
 (async () => {
-	let buffer = Buffer.from(`KoZIhvcNAQEL`, `base64`);
-	let parser = new parsing.Parser(buffer);
-	let id = oid.parse(parser);
-	console.log(id.join(`.`) === `1.2.840.113549.1.1.11`);
+	let parser = new parsing.Parser(Buffer.from(`KoZIhvcNAQEL`, `base64`));
+	let observed = oid.parse(parser).join(`.`);
+	let expected = `1.2.840.113549.1.1.11`;
+	console.assert(observed === expected, `It should parse a common cryptographic oid.`);
 })();
 
 (async () => {
-	let buffer = oid.serialize([0, 39]);
-	console.log(buffer.equals(Buffer.of(0x27)));
-	let id = oid.parse(new parsing.Parser(buffer));
-	console.log(id.join(`.`) === `0.39`);
+	let observed = oid.serialize([1, 2, 840, 113549, 1, 1, 11]);
+	let expected = Buffer.from(`KoZIhvcNAQEL`, `base64`);
+	console.assert(observed.equals(expected), `It should serialize a common cryptographic oid.`);
 })();
 
 (async () => {
-	let unexpected = `Expected method to throw!`;
+	let parser = new parsing.Parser(Buffer.of(0x27));
+	let observed = oid.parse(parser).join(`.`);
+	let expected = `0.39`;
+	console.assert(observed === expected, `It should parse [0, 39] properly.`);
+})();
+
+(async () => {
+	let observed = oid.serialize([0, 39]);
+	let expected = Buffer.of(0x27);
+	console.assert(observed.equals(expected), `It should serialize [0, 39] properly.`);
+})();
+
+(async () => {
 	try {
 		let buffer = oid.serialize([0, 40]);
-		throw unexpected;
-	} catch (error) {
-		if (error === unexpected) {
-			throw error;
-		}
-	}
+		console.assert(false, `It should throw an error when serializing [0, 40].`);
+	} catch (error) {}
 })();
 
 (async () => {
-	let buffer = oid.serialize([1, 39]);
-	console.log(buffer.equals(Buffer.of(0x4F)));
-	let id = oid.parse(new parsing.Parser(buffer));
-	console.log(id.join(`.`) === `1.39`);
+	let parser = new parsing.Parser(Buffer.of(0x4F));
+	let observed = oid.parse(parser).join(`.`);
+	let expected = `1.39`;
+	console.assert(observed === expected, `It should parse [1, 39] properly.`);
 })();
 
 (async () => {
-	let unexpected = `Expected method to throw!`;
+	let observed = oid.serialize([1, 39]);
+	let expected = Buffer.of(0x4F);
+	console.assert(observed.equals(expected), `It should serialize [1, 39] properly.`);
+})();
+
+(async () => {
 	try {
 		let buffer = oid.serialize([1, 40]);
-		throw unexpected;
-	} catch (error) {
-		if (error === unexpected) {
-			throw error;
-		}
-	}
+		console.assert(false, `It should throw an error when serializing [1, 40].`);
+	} catch (error) {}
 })();
 
 (async () => {
-	let buffer = oid.serialize([2, 47]);
-	console.log(buffer.equals(Buffer.of(0x7F)));
-	let id = oid.parse(new parsing.Parser(buffer));
-	console.log(id.join(`.`) === `2.47`);
+	let parser = new parsing.Parser(Buffer.of(0x7F));
+	let observed = oid.parse(parser).join(`.`);
+	let expected = `2.47`;
+	console.assert(observed === expected, `It should parse [2, 47] properly.`);
 })();
 
 (async () => {
-	let buffer = oid.serialize([2, 48]);
-	console.log(buffer.equals(Buffer.of(0x81, 0x00)));
-	let id = oid.parse(new parsing.Parser(buffer));
-	console.log(id.join(`.`) === `2.48`);
+	let observed = oid.serialize([2, 47]);
+	let expected = Buffer.of(0x7F);
+	console.assert(observed.equals(expected), `It should serialize [2, 47] properly.`);
 })();
 
 (async () => {
-	let buffer = oid.serialize([2, 999]);
-	console.log(buffer.equals(Buffer.of(0x88, 0x37)));
-	let id = oid.parse(new parsing.Parser(buffer));
-	console.log(id.join(`.`) === `2.999`);
+	let parser = new parsing.Parser(Buffer.of(0x81, 0x00));
+	let observed = oid.parse(parser).join(`.`);
+	let expected = `2.48`;
+	console.assert(observed === expected, `It should parse [2, 48] properly.`);
+})();
+
+(async () => {
+	let observed = oid.serialize([2, 48]);
+	let expected = Buffer.of(0x81, 0x00);
+	console.assert(observed.equals(expected), `It should serialize [2, 48] properly.`);
 })();
