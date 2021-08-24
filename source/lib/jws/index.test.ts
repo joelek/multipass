cfe35edd203a1df132fa81b1892aa0a4740dda7e
Joel Ek <contact@joelek.se>
2021-08-24 12:43:15 +0200

    Clean up JWS tests.

--- source/lib/jws/index.test.ts
+++ source/lib/jws/index.test.ts
@@ -1,20 +1,49 @@
-import * as libcrypto from "crypto";
 import * as jws from "./";
 
+const PRIVATE_KEY =
+`
+-----BEGIN RSA PRIVATE KEY-----
+MIIBOwIBAAJBANqajvX0e7/wnenef61SwR20VURtve8dE5N6onEMr0D5SZUZWX8S
+Luqfa928/MsXI4Ci55UreOXWC8of4cMu0e8CAwEAAQJBALHzIS8cfuRHVfT8F4kb
+FXM9yi9y+is8yyPBr5xBTCShiFlAXpSkYkadmYyLi3sDn72y4moSZPI7QjkGYfuV
+Y/ECIQDucHHYYH+d+WeeofidLk2bOhhuhbTHd/qroOkjsVYCdwIhAOq0Ii5fWonJ
+naVsdTrvI+BmbelAbLFW+e0TNZrrklJJAiBcxmvFoWaGdTCYTLWLkySnLWesOWIp
+6skiVq3gMXQh6QIgF7YdIew2PGdnCthbO5n/WONgRUlh8cSkuUPQjZcxECkCIQDQ
+fah6lbBm4rufCWqw1QuNS0/IVXgOUovxLifhd/VhZQ==
+-----END RSA PRIVATE KEY-----
+`;
+
+const PUBLIC_KEY =
+`
+-----BEGIN RSA PUBLIC KEY-----
+MEgCQQDamo719Hu/8J3p3n+tUsEdtFVEbb3vHROTeqJxDK9A+UmVGVl/Ei7qn2vd
+vPzLFyOAoueVK3jl1gvKH+HDLtHvAgMBAAE=
+-----END RSA PUBLIC KEY-----
+`;
+
 (async () => {
-	let { privateKey, publicKey } = libcrypto.generateKeyPairSync(`rsa`, {
-		modulusLength: 4096,
-		publicExponent: 65537,
-		publicKeyEncoding: {
-			type: `pkcs1`,
-			format: `pem`
-		},
-		privateKeyEncoding: {
-			type: `pkcs1`,
-			format: `pem`
-		}
-	});
-	let blob = await jws.sign(privateKey, { food: "räksmörgås" }, "räksmörgås");
-	let verification = await jws.verify(blob, publicKey);
-	console.log(verification);
+	let blob = await jws.sign(PRIVATE_KEY, { food: `räksmörgås` }, `räksmörgås`);
+	let observed = blob.protected;
+	let expected = `eyJmb29kIjoicsOka3Ntw7ZyZ8OlcyIsImFsZyI6IlJTMjU2In0`;
+	console.assert(observed === expected, `It should encode the protected section properly.`);
+})();
+
+(async () => {
+	let blob = await jws.sign(PRIVATE_KEY, { food: `räksmörgås` }, `räksmörgås`);
+	let observed = blob.payload;
+	let expected = `InLDpGtzbcO2cmfDpXMi`;
+	console.assert(observed === expected, `It should encode the payload section properly.`);
+})();
+
+(async () => {
+	let blob = await jws.sign(PRIVATE_KEY, { food: `räksmörgås` }, `räksmörgås`);
+	let observed = blob.signature;
+	let expected = `cPl6tjXiYHgHC7uvUBe66VGD3zdg3GSm3v_tGWlkYfpVUGUcA7JQ7SF9u5mEPr3sI2sje8ycRi2Dm1qJl3U_hA`;
+	console.assert(observed === expected, `It should sign the protected and payload sections properly.`);
+})();
+
+(async () => {
+	let blob = await jws.sign(PRIVATE_KEY, { food: `räksmörgås` }, `räksmörgås`);
+	let verification = await jws.verify(blob, PUBLIC_KEY);
+	console.assert(verification, `It should verify the signature properly.`);
 })();
