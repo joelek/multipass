0e382669668fd3fcc39ffd2e056317f03876b58b
Joel Ek <contact@joelek.se>
2021-08-26 20:51:14 +0200

    Clean up JWK module.

--- source/lib/jwk/index.ts
+++ source/lib/jwk/index.ts
@@ -1,54 +1,42 @@
-import * as asno from "../der";
-import * as encoding from "../encoding";
-import * as $fs from "../fs";
-
-type WebKey = {
-	kty: "RSA",
-	n: string,
-	e: string
+export enum KeyType {
+	"EC",
+	"RSA",
+	"oct"
 };
 
-class Key {
-	private private_key: Buffer;
+export enum Curve {
+	"P-256",
+	"P-384",
+	"P-521"
+};
 
-	constructor(private_key: Buffer) {
-		this.private_key = private_key;
-	}
+export type PublicKeyEC = {
+	kty: `EC`;
+	crv: keyof typeof Curve;
+	x: string;
+	y: string;
+};
 
-	async getWebKey(): Promise<WebKey> {
-		let nodes = await asno.parse(this.private_key);
-		let data = nodes[0].data;
-		if (data instanceof Buffer) {
-			return Promise.reject("Unexpected buffer!");
-		}
-		let n = (data[1] as any).data;
-		if (!(n instanceof Buffer)) {
-			return Promise.reject("Expected a buffer!");
-		}
-		let e = (data[2] as any).data;
-		if (!(e instanceof Buffer)) {
-			return Promise.reject("Expected a buffer!");
-		}
-		return {
-			kty: "RSA",
-			n: await encoding.convertBufferToBase64URLString(n),
-			e: await encoding.convertBufferToBase64URLString(e)
-		};
-	}
+export type PrivateKeyEC = PublicKeyEC & {
+	d: string;
+};
 
-	async save(path: string): Promise<void> {
-		return $fs.writeBufferToFile(path, this.private_key);
-	}
+export type PublicKeyRSA = {
+	kty: `RSA`;
+	n: string;
+	e: string;
+};
 
-	static async load(path: string): Promise<Key> {
-		return Promise.resolve(path)
-			.then($fs.readFileToBuffer)
-			.then((buffer) => {
-				return new Key(buffer);
-			});
-	}
-}
-
-export {
-	Key
+export type PrivateKeyRSA = PublicKeyRSA & {
+	d: string;
+	p?: string;
+	q?: string;
+	dp?: string;
+	dq?: string;
+	qi?: string;
+	oth?: Array<{
+		r: string;
+		d: string;
+		t: string;
+	}>
 };
