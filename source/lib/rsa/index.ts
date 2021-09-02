dc5f4381910d2655e8469b385b7c9a6828d88200
Joel Ek <contact@joelek.se>
2021-09-02 12:09:55 +0200

    Use Autoguard to define PKCS schema.

--- source/lib/rsa/index.ts
+++ source/lib/rsa/index.ts
@@ -1,18 +1,14 @@
 import * as libcrypto from "crypto";
-import * as asno from "../asno";
 import * as der from "../der";
-import * as oid from "../oid";
 import * as parsing from "../parsing";
+import * as schema from "./schema";
 
 export type PublicKey = {
 	modulus: Buffer;
 	public_exponent: Buffer;
 };
 
-export type PrivateKey = {
-	version: Buffer;
-	modulus: Buffer;
-	public_exponent: Buffer;
+export type PrivateKey = PublicKey & {
 	private_exponent: Buffer;
 	prime_one: Buffer;
 	prime_two: Buffer;
@@ -42,62 +38,16 @@ export function generateDerPrivateKey(options?: Partial<{
 };
 
 export function parsePKCS1(parser: parsing.Parser): PrivateKey {
-	let node = asno.expect(der.parseNode(parser), {
-		kind: `UNIVERSAL`,
-		form: `CONSTRUCTED`,
-		type: `SEQUENCE`
-	});
-	let children = der.parse(new parsing.Parser(node.data));
-	if (children.length < 9) {
-		throw `Expected at least 9 children!`;
-	}
-	let version = asno.expect(children[0], {
-		kind: `UNIVERSAL`,
-		form: `PRIMITIVE`,
-		type: `INTEGER`
-	}).data;
-	let modulus = asno.expect(children[1], {
-		kind: `UNIVERSAL`,
-		form: `PRIMITIVE`,
-		type: `INTEGER`
-	}).data;
-	let public_exponent = asno.expect(children[2], {
-		kind: `UNIVERSAL`,
-		form: `PRIMITIVE`,
-		type: `INTEGER`
-	}).data;
-	let private_exponent = asno.expect(children[3], {
-		kind: `UNIVERSAL`,
-		form: `PRIMITIVE`,
-		type: `INTEGER`
-	}).data;
-	let prime_one = asno.expect(children[4], {
-		kind: `UNIVERSAL`,
-		form: `PRIMITIVE`,
-		type: `INTEGER`
-	}).data;
-	let prime_two = asno.expect(children[5], {
-		kind: `UNIVERSAL`,
-		form: `PRIMITIVE`,
-		type: `INTEGER`
-	}).data;
-	let exponent_one = asno.expect(children[6], {
-		kind: `UNIVERSAL`,
-		form: `PRIMITIVE`,
-		type: `INTEGER`
-	}).data;
-	let exponent_two = asno.expect(children[7], {
-		kind: `UNIVERSAL`,
-		form: `PRIMITIVE`,
-		type: `INTEGER`
-	}).data;
-	let coefficient = asno.expect(children[8], {
-		kind: `UNIVERSAL`,
-		form: `PRIMITIVE`,
-		type: `INTEGER`
-	}).data;
+	let node = schema.RSAPrivateKey.as(der.parseNode(parser));
+	let modulus = Buffer.from(node.data[1].data, `base64`);
+	let public_exponent = Buffer.from(node.data[2].data, `base64`);
+	let private_exponent = Buffer.from(node.data[3].data, `base64`);
+	let prime_one = Buffer.from(node.data[4].data, `base64`);
+	let prime_two = Buffer.from(node.data[5].data, `base64`);
+	let exponent_one = Buffer.from(node.data[6].data, `base64`);
+	let exponent_two = Buffer.from(node.data[7].data, `base64`);
+	let coefficient = Buffer.from(node.data[8].data, `base64`);
 	return {
-		version,
 		modulus,
 		public_exponent,
 		private_exponent,
@@ -110,47 +60,9 @@ export function parsePKCS1(parser: parsing.Parser): PrivateKey {
 };
 
 export function parsePKCS8(parser: parsing.Parser): PrivateKey {
-	let node = asno.expect(der.parseNode(parser), {
-		kind: `UNIVERSAL`,
-		form: `CONSTRUCTED`,
-		type: `SEQUENCE`
-	});
-	let children = der.parse(new parsing.Parser(node.data));
-	if (children.length < 3) {
-		throw `Expected at least 3 children!`;
-	}
-	let version = asno.expect(children[0], {
-		kind: `UNIVERSAL`,
-		form: `PRIMITIVE`,
-		type: `INTEGER`
-	}).data;
-	let algorithms = der.parse(new parsing.Parser(asno.expect(children[1], {
-		kind: `UNIVERSAL`,
-		form: `CONSTRUCTED`,
-		type: `SEQUENCE`
-	}).data));
-	if (algorithms.length < 2) {
-		throw `Expected at least 2 children!`;
-	}
-	let rsa = oid.parse(new parsing.Parser(asno.expect(algorithms[0], {
-		kind: `UNIVERSAL`,
-		form: `PRIMITIVE`,
-		type: `OBJECT_IDENTIFIER`
-	}).data)).join(`.`);
-	if (rsa !== oid.RSA) {
-		throw `Expected an RSA object identifier!`;
-	}
-	let terminator = asno.expect(algorithms[1], {
-		kind: `UNIVERSAL`,
-		form: `PRIMITIVE`,
-		type: `NULL`
-	}).data;
-	let private_key = asno.expect(children[2], {
-		kind: `UNIVERSAL`,
-		form: `PRIMITIVE`,
-		type: `OCTET_STRING`
-	}).data;
-	return parsePKCS1(new parsing.Parser(private_key));
+	let node = schema.PrivateKeyInfo.as(der.parseNode(parser));
+	let pkcs1 = Buffer.from(node.data[2].data, `base64`);
+	return parsePKCS1(new parsing.Parser(pkcs1));
 };
 
 export function generatePrivateKey(): PrivateKey {
