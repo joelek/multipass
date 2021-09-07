c376bfa2dd7876bc6665e05ce86ce3ba0c73b1ce
Joel Ek <contact@joelek.se>
2021-09-07 16:20:04 +0200

    PKCS8: Add support for parsing private RSA keys as public RSA keys.

--- source/lib/pkcs8/index.ts
+++ source/lib/pkcs8/index.ts
@@ -8,6 +8,14 @@ import * as parsing from "../parsing";
 export * from "./schema";
 
 export function parseRSAPublicKey(buffer: Buffer, passphrase?: string): jwk.RSAPublicKey {
+	try {
+		let { n, e } = parseRSAPrivateKey(buffer, passphrase);
+		return {
+			kty: "RSA",
+			n,
+			e
+		};
+	} catch (error) {}
 	let bufferPKCS8 = (passphrase != null) ? pkcs5.decrypt(buffer, passphrase) : buffer;
 	let parser = new parsing.Parser(bufferPKCS8);
 	let node = pkcs8.RSAPublicKey.as(der.parseNode(parser));
