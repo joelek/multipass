d4e5e2d49beac4491bf05addb3f6c43d16ce82a9
Joel Ek <contact@joelek.se>
2021-09-07 22:58:53 +0200

    PKCS8: Externalise encryption features.

--- source/lib/pkcs8/index.ts
+++ source/lib/pkcs8/index.ts
@@ -8,23 +8,22 @@ import * as parsing from "../parsing";
 
 export * from "./schema";
 
-export function parseRSAPublicKey(buffer: Buffer, passphrase?: string): jwk.RSAPublicKey {
+export function parseRSAPublicKey(bufferPKCS8: Buffer): jwk.RSAPublicKey {
 	try {
-		let { n, e } = parseRSAPrivateKey(buffer, passphrase);
+		let { n, e } = parseRSAPrivateKey(bufferPKCS8);
 		return {
 			kty: "RSA",
 			n,
 			e
 		};
 	} catch (error) {}
-	let bufferPKCS8 = (passphrase != null) ? pkcs5.decrypt(buffer, passphrase) : buffer;
 	let parser = new parsing.Parser(bufferPKCS8);
 	let node = schema.RSAPublicKey.as(der.parseNode(parser));
 	let bufferPKCS1 = Buffer.from(node.data[1].data, "base64url");
 	return pkcs1.parseRSAPublicKey(bufferPKCS1);
 };
 
-export function serializeRSAPublicKey(key: jwk.RSAPublicKey, passphrase?: string): Buffer {
+export function serializeRSAPublicKey(key: jwk.RSAPublicKey): Buffer {
 	let bufferPKCS1 = pkcs1.serializeRSAPublicKey(key);
 	let node: schema.RSAPublicKey = {
 		...asn1.SEQUENCE,
@@ -49,19 +48,17 @@ export function serializeRSAPublicKey(key: jwk.RSAPublicKey, passphrase?: string
 		]
 	};
 	let bufferPKCS8 = der.serializeNode(node);
-	// TODO: Encrypt.
 	return bufferPKCS8;
 };
 
-export function parseRSAPrivateKey(buffer: Buffer, passphrase?: string): jwk.RSAPrivateKey {
-	let bufferPKCS8 = (passphrase != null) ? pkcs5.decrypt(buffer, passphrase) : buffer;
+export function parseRSAPrivateKey(bufferPKCS8: Buffer): jwk.RSAPrivateKey {
 	let parser = new parsing.Parser(bufferPKCS8);
 	let node = schema.RSAPrivateKey.as(der.parseNode(parser));
 	let bufferPKCS1 = Buffer.from(node.data[2].data, "base64url");
 	return pkcs1.parseRSAPrivateKey(bufferPKCS1);
 };
 
-export function serializeRSAPrivateKey(key: jwk.RSAPrivateKey, passphrase?: string): Buffer {
+export function serializeRSAPrivateKey(key: jwk.RSAPrivateKey): Buffer {
 	let bufferPKCS1 = pkcs1.serializeRSAPrivateKey(key);
 	let node: schema.RSAPrivateKey = {
 		...asn1.SEQUENCE,
@@ -90,6 +87,5 @@ export function serializeRSAPrivateKey(key: jwk.RSAPrivateKey, passphrase?: stri
 		]
 	};
 	let bufferPKCS8 = der.serializeNode(node);
-	// TODO: Encrypt.
 	return bufferPKCS8;
 };
