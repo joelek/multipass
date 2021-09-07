d7ffbb8312ea672ed9ba2322ef16cbf8e42bbab4
Joel Ek <contact@joelek.se>
2021-09-07 14:34:27 +0200

    ASN1: Add additional helper constants.

--- source/lib/asn1/index.ts
+++ source/lib/asn1/index.ts
@@ -1,4 +1,5 @@
 import * as schema from "./schema";
+
 export * from "./schema";
 
 export const SEQUENCE: schema.Sequence = {
@@ -12,5 +13,33 @@ export const INTEGER: schema.Integer = {
 	kind: "UNIVERSAL",
 	form: "PRIMITIVE",
 	type: "INTEGER",
+	data: ""
+};
+
+export const OBJECT_IDENTIFER: schema.ObjectIdentifier = {
+	kind: "UNIVERSAL",
+	form: "PRIMITIVE",
+	type: "OBJECT_IDENTIFIER",
+	data: ""
+};
+
+export const NULL: schema.Null = {
+	kind: "UNIVERSAL",
+	form: "PRIMITIVE",
+	type: "NULL",
+	data: ""
+};
+
+export const OCTET_STRING: schema.OctetString = {
+	kind: "UNIVERSAL",
+	form: "PRIMITIVE",
+	type: "OCTET_STRING",
+	data: ""
+};
+
+export const BIT_STRING: schema.BitString = {
+	kind: "UNIVERSAL",
+	form: "PRIMITIVE",
+	type: "BIT_STRING",
 	data: ""
 };
