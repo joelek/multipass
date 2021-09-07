19db7b935715224fa4783fb0048f86f01d49aafa
Joel Ek <contact@joelek.se>
2021-09-07 16:18:52 +0200

    PKCS8: Add support for encrypted public keys.

--- source/lib/pkcs8/index.ts
+++ source/lib/pkcs8/index.ts
@@ -7,8 +7,9 @@ import * as parsing from "../parsing";
 
 export * from "./schema";
 
-export function parseRSAPublicKey(buffer: Buffer): jwk.RSAPublicKey {
-	let parser = new parsing.Parser(buffer);
+export function parseRSAPublicKey(buffer: Buffer, passphrase?: string): jwk.RSAPublicKey {
+	let bufferPKCS8 = (passphrase != null) ? pkcs5.decrypt(buffer, passphrase) : buffer;
+	let parser = new parsing.Parser(bufferPKCS8);
 	let node = pkcs8.RSAPublicKey.as(der.parseNode(parser));
 	let bufferPKCS1 = Buffer.from(node.data[1].data, "base64url");
 	return pkcs1.parseRSAPublicKey(bufferPKCS1);
