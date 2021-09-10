41f68f10b7948d794055ddfb870a703e9af15467
Joel Ek <contact@joelek.se>
2021-09-10 11:19:36 +0200

    EC: Add module.

---
+++ source/lib/ec/index.ts
@@ -0,0 +1,29 @@
+import * as libcrypto from "crypto";
+import * as jwk from "../jwk";
+
+export function generatePrivateKeyDER(options?: Partial<{
+	namedCurve: "prime256v1" | "secp384r1" | "secp521r1" | string,
+	type: "pkcs8" | "sec1"
+}>): Buffer {
+	let namedCurve = options?.namedCurve ?? "prime256v1";
+	let type = options?.type ?? "pkcs8";
+	let pair = libcrypto.generateKeyPairSync("ec", {
+		namedCurve: namedCurve,
+		publicKeyEncoding: {
+			type: "spki",
+			format: "der"
+		},
+		privateKeyEncoding: {
+			type: type,
+			format: "der"
+		}
+	});
+	return pair.privateKey;
+};
+
+export function generatePrivateKey(): jwk.ECPrivateKey {
+	let buffer = generatePrivateKeyDER({
+		type: "sec1"
+	});
+	throw `TODO`;
+};
