5c6db77cbc253689dd97259cace34d1f9176db69
Joel Ek <contact@joelek.se>
2021-09-15 10:42:52 +0200

    JWK: Add computeThumbprint().

--- source/lib/jwk/index.ts
+++ source/lib/jwk/index.ts
@@ -1,3 +1,4 @@
+import * as libcrypto from "crypto";
 import * as asn1 from "../asn1";
 import * as schema from "./schema";
 
@@ -34,6 +35,23 @@ export function getPublicKey(key: schema.AssymetricKey): schema.PublicKey {
 			x,
 			y
 		};
+	}
+	throw `Expected code to be unreachable!`;
+};
+
+export function computeThumbprint(key: schema.AssymetricKey): string {
+	let hash = libcrypto.createHash("sha256");
+	if (schema.RSAPublicKey.is(key)) {
+		let { kty, n, e } = key;
+		hash.update(Buffer.from(`{"e":"${e}","kty":"${kty}","n":"${n}"}`));
+		let buffer = hash.digest();
+		return buffer.toString("base64url");
+	}
+	if (schema.ECPublicKey.is(key)) {
+		let { kty, crv, x, y } = key;
+		hash.update(Buffer.from(`{"crv":"${crv}","kty":"${kty}","x":"${x}","y":"${y}"}`));
+		let buffer = hash.digest();
+		return buffer.toString("base64url");
 	}
 	throw `Expected code to be unreachable!`;
 };
