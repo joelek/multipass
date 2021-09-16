fc241634b51f1835c6962c4224e109ed31514e7b
Joel Ek <contact@joelek.se>
2021-09-16 13:34:10 +0200

    ACME: Add computeKeyAuthorization().

--- source/lib/acme/index.ts
+++ source/lib/acme/index.ts
@@ -1,3 +1,14 @@
+import * as libcrypto from "crypto";
+import * as jwk from "../jwk";
+
 export * as api from "./api";
 export * as config from "./config";
 export * as handler from "./handler";
+
+export function computeKeyAuthorization(token: string, key: jwk.AssymetricKey): string {
+	let thumbprint = jwk.computeThumbprint(key);
+	let hash = libcrypto.createHash("sha256");
+	hash.update(Buffer.from(`${token}.${thumbprint}`));
+	let buffer = hash.digest();
+	return buffer.toString("base64url");
+};
