15d63811694fd95e9d79e57f98afc581d2f4a479
Joel Ek <contact@joelek.se>
2021-09-18 17:36:11 +0200

    ACME: Remove old config.

--- source/mod/acme/index.ts
+++ source/mod/acme/index.ts
@@ -2,7 +2,6 @@ import * as libcrypto from "crypto";
 import * as jwk from "../jwk";
 
 export * as api from "./api";
-export * as config from "./config";
 export * as handler from "./handler";
 
 export function computeKeyAuthorization(token: string, key: jwk.AssymetricKey): string {
