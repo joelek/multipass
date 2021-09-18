5d9f8d45cbd8fe9fca0b7d05669bcc42a3bffd34
Joel Ek <contact@joelek.se>
2021-09-18 18:21:23 +0200

    Add cli implementation.

--- source/cli/index.ts
+++ source/cli/index.ts
@@ -1,1 +1,85 @@
 #!/usr/bin/env node
+
+import * as lib from "../lib";
+
+const LETS_ENCRYPT_STAGING = "https://acme-staging-v02.api.letsencrypt.org/directory";
+const LETS_ENCRYPT = "https://acme-v02.api.letsencrypt.org/directory";
+
+async function run(): Promise<number> {
+	let certificate: lib.config.Certificate = {
+		hostnames: [],
+	};
+	let options: lib.config.Options = {
+		providers: [],
+		certificates: [],
+		acme: LETS_ENCRYPT_STAGING
+	};
+	let found_unrecognized_argument = false;
+	for (let argv of process.argv.slice(2)) {
+		let parts: RegExpExecArray | null = null;
+		if ((parts = /^--acme=(.*)$/.exec(argv)) != null) {
+			options.acme = parts[1];
+		} else if ((parts = /^--config=(.*)$/.exec(argv)) !== null) {
+			options = lib.loadConfig(parts[1]);
+			break;
+		} else if ((parts = /^--dns=dynu[:](.*)$/.exec(argv)) != null) {
+			options.providers.push({
+				type: "dynu",
+				key: parts[1]
+			});
+		} else if ((parts = /^--dns=glesys[:](.*)[:](.*)$/.exec(argv)) != null) {
+			options.providers.push({
+				type: "glesys",
+				account: parts[1],
+				key: parts[2]
+			});
+		} else if ((parts = /^--hostname=(.*)$/.exec(argv)) != null) {
+			certificate.hostnames.push(parts[1]);
+		} else if ((parts = /^--monitor=(true|false)$/.exec(argv)) != null) {
+			options.monitor = parts[1] === "true";
+		} else if ((parts = /^--root=(.*)$/.exec(argv)) != null) {
+			certificate.root = parts[1];
+			options.certificates.push(certificate);
+			certificate = {
+				hostnames: []
+			};
+		} else {
+			found_unrecognized_argument = true;
+			console.log(`Unrecognized argument "${argv}"!`);
+		}
+	}
+	if (found_unrecognized_argument) {
+		console.log(`Arguments:`);
+		console.log(`	--acme=string`);
+		console.log(`		Set ACME directory URL.`);
+		console.log(`	--config=string`);
+		console.log(`		Load specified config.`);
+		console.log(`	--dns=string`);
+		console.log(`		Set DNS provider and associated credentials.`);
+		console.log(`	--hostname=string`);
+		console.log(`		Set hostname for which to request certificate.`);
+		console.log(`	--monitor=boolean`);
+		console.log(`		Configure automatic monitoring and renewal of certificates.`);
+		console.log(`	--root=string`);
+		console.log(`		Set directory for which to store associated files.`);
+		return 1;
+	} else {
+		if (options.certificates.length === 0) {
+			options.certificates.push(certificate);
+		}
+		if (options.acme === "le") {
+			options.acme = LETS_ENCRYPT;
+		}
+		await lib.run(options);
+		return 0;
+	}
+};
+
+run()
+	.catch((error) => {
+		console.log(String(error));
+		return 1;
+	})
+	.then((code) => {
+		process.exit(code);
+	});
