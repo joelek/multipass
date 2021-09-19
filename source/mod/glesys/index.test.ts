cfc9e1de26ed367efdf85124b5bdc20df121e7f4
Joel Ek <contact@joelek.se>
2021-09-19 13:06:48 +0200

    GLESYS: Add support for custom client options.

--- source/mod/glesys/index.test.ts
+++ source/mod/glesys/index.test.ts
@@ -1,15 +1,16 @@
 import * as libfs from "fs";
 import * as glesys from "./";
 
-async function test(): Promise<void> {
+(async () => {
 	let config = glesys.config.Config.as(JSON.parse(libfs.readFileSync("./private/config/glesys.json", "utf-8")));
-	let client = glesys.makeClient(config);
+	let client = glesys.makeClient(config, {
+		debugMode: true
+	});
 	let domains = await (await client.listDomains({})).payload();
 	let domain = domains.response.domains.pop();
 	if (domain == null) {
 		throw "Expected a domain!";
 	}
-	console.log(JSON.stringify({ domain }, null, "\t"));
 	let created = await (await client.createDomainRecord({
 		payload: {
 			domainname: domain.domainname,
@@ -19,7 +20,6 @@ async function test(): Promise<void> {
 			ttl: 60
 		}
 	})).payload();
-	console.log(JSON.stringify({ created }, null, "\t"));
 	let updated = await (await client.updateDomainRecord({
 		payload: {
 			recordid: created.response.record.recordid,
@@ -29,13 +29,9 @@ async function test(): Promise<void> {
 			ttl: 60
 		}
 	})).payload();
-	console.log(JSON.stringify({ updated }, null, "\t"));
 	let deleted = await (await client.deleteDomainRecord({
 		payload: {
 			recordid: created.response.record.recordid
 		}
 	})).payload();
-	console.log(JSON.stringify({ deleted }, null, "\t"));
-}
-
-test().catch((error) => console.log(String(error)));
+})();
--- source/mod/glesys/index.ts
+++ source/mod/glesys/index.ts
@@ -6,14 +6,15 @@ export * as config from "./config";
 
 const URL_PREFIX = "https://api.glesys.com";
 
-export function makeClient(config: config.Config): api.Client {
+export function makeClient(config: config.Config, options?: autoguard.api.ClientOptions): api.Client {
 	let token = Buffer.from(`${config.account}:${config.key}`).toString("base64");
 	let client = api.makeClient({
 		urlPrefix: URL_PREFIX,
 		requestHandler: autoguard.api.makeNodeRequestHandler(),
 		defaultHeaders: [
 			["Authorization", `Basic ${token}`]
-		]
+		],
+		...options
 	});
 	return client;
 };
