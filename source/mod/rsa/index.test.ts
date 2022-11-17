93f4c7adb0fe33334afdd47e9d4c0d31ed6a9b72
Joel Ek <contact@joelek.se>
2022-11-17 13:33:50 +0100

    rsa: Add more options for key generation.

--- source/mod/rsa/index.test.ts
+++ source/mod/rsa/index.test.ts
@@ -6,13 +6,58 @@ import * as rsa from "./";
 })();
 
 (async () => {
-	let key = rsa.generatePrivateKeyPKCS1();
-	console.assert(true, `It should generate keys using the PKCS1 format properly.`);
+	let key = rsa.generatePrivateKeyPKCS1({
+		format: "der"
+	});
+	console.assert(true, `It should generate keys using the PKCS1 container and the DER format.`);
 })();
 
 (async () => {
-	let key = rsa.generatePrivateKeyPKCS8();
-	console.assert(true, `It should generate keys using the PKCS8 format properly.`);
+	let key = rsa.generatePrivateKeyPKCS1({
+		format: "pem"
+	});
+	console.assert(true, `It should generate keys using the PKCS1 container and the PEM format.`);
+})();
+
+(async () => {
+	let key = rsa.generatePrivateKeyPKCS1({
+		format: "pem",
+		passphrase: "test",
+		cipher: "aes-256-cbc"
+	});
+	console.assert(true, `It should generate encrypted keys using the PKCS1 container and the PEM format.`);
+})();
+
+(async () => {
+	let key = rsa.generatePrivateKeyPKCS8({
+		format: "der"
+	});
+	console.assert(true, `It should generate keys using the PKCS8 container and the DER format.`);
+})();
+
+(async () => {
+	let key = rsa.generatePrivateKeyPKCS8({
+		format: "der",
+		passphrase: "test",
+		cipher: "aes-256-cbc"
+	});
+	console.assert(true, `It should generate encrypted keys using the PKCS8 container and the DER format.`);
+})();
+
+(async () => {
+	let key = rsa.generatePrivateKeyPKCS8({
+		format: "pem"
+	});
+	console.assert(true, `It should generate keys using the PKCS8 container and the PEM format.`);
+})();
+
+(async () => {
+	let key = rsa.generatePrivateKeyPKCS8({
+		format: "pem",
+		passphrase: "test",
+		cipher: "aes-256-cbc"
+	});
+	console.assert(true, `It should generate encrypted keys using the PKCS8 container and the PEM format.`);
 })();
 
 (async () => {
--- source/mod/rsa/index.ts
+++ source/mod/rsa/index.ts
@@ -1,9 +1,39 @@
 import * as libcrypto from "crypto";
 import * as jwk from "../jwk";
 
-export function generatePrivateKey(options?: Partial<{
-	modulusLength: number
-}>): libcrypto.KeyObject {
+const DEFAULT_CIPHER = libcrypto.getCiphers()[0];
+
+export type RSAKeyOptions = {
+	modulusLength?: number;
+};
+
+export type RSAExportOptionsPKCS1 = {
+	format: "der";
+} | {
+	format: "pem";
+	passphrase?: string;
+	cipher?: string;
+};
+
+export type RSAExportOptionsPKCS8 = {
+	format: "der";
+	passphrase?: string;
+	cipher?: string;
+} | {
+	format: "pem";
+	passphrase?: string;
+	cipher?: string;
+};
+
+export type RSAExportOptions = ({
+	container: "pkcs1";
+} & RSAExportOptionsPKCS1) | ({
+	container: "pkcs8";
+} & RSAExportOptionsPKCS8);
+
+export type RSAOptions = RSAKeyOptions & RSAExportOptions;
+
+export function generatePrivateKey(options?: RSAKeyOptions): libcrypto.KeyObject {
 	let modulusLength = options?.modulusLength ?? 4096;
 	let pair = libcrypto.generateKeyPairSync("rsa", {
 		modulusLength: modulusLength
@@ -11,32 +41,63 @@ export function generatePrivateKey(options?: Partial<{
 	return pair.privateKey;
 };
 
-export function generatePrivateKeyPKCS1(options?: Partial<{
-	modulusLength: number
-}>): Buffer {
+export function generatePrivateKeyPKCS1(options?: RSAKeyOptions & RSAExportOptionsPKCS1): Buffer {
 	let key = generatePrivateKey(options);
-	return key.export({
-		format: "der",
-		type: "pkcs1"
-	});
+	if (options?.format === "pem") {
+		let passphrase = options.passphrase;
+		let cipher = options?.cipher ?? (typeof passphrase === "undefined" ? undefined : DEFAULT_CIPHER);
+		return Buffer.from(key.export({
+			format: "pem",
+			type: "pkcs1",
+			cipher: cipher,
+			passphrase: passphrase
+		}));
+	} else {
+		return key.export({
+			format: "der",
+			type: "pkcs1"
+		});
+	}
 };
 
-export function generatePrivateKeyPKCS8(options?: Partial<{
-	modulusLength: number
-}>): Buffer {
+export function generatePrivateKeyPKCS8(options?: RSAKeyOptions & RSAExportOptionsPKCS8): Buffer {
 	let key = generatePrivateKey(options);
-	return key.export({
-		format: "der",
-		type: "pkcs8"
-	});
+	if (options?.format === "pem") {
+		let passphrase = options?.passphrase;
+		let cipher = options?.cipher ?? (typeof passphrase === "undefined" ? undefined : DEFAULT_CIPHER);
+		return Buffer.from(key.export({
+			format: "pem",
+			type: "pkcs8",
+			cipher: cipher,
+			passphrase: passphrase
+		}));
+	} else {
+		let passphrase = options?.passphrase;
+		let cipher = options?.cipher ?? (typeof passphrase === "undefined" ? undefined : DEFAULT_CIPHER);
+		return key.export({
+			format: "der",
+			type: "pkcs8",
+			cipher: cipher,
+			passphrase: passphrase
+		});
+	}
 };
 
-export function generatePrivateKeyJWK(options?: Partial<{
-	modulusLength: number
-}>): jwk.RSAPrivateKey {
+export function generatePrivateKeyJWK(options?: RSAKeyOptions): jwk.RSAPrivateKey {
 	let key = generatePrivateKey(options);
 	let json = key.export({
 		format: "jwk"
 	});
 	return jwk.RSAPrivateKey.as(json);
+};
+
+export function generatePrivateKeyBuffer(options?: RSAOptions): Buffer {
+	if (options?.container === "pkcs8") {
+		return generatePrivateKeyPKCS8(options);
+	} else {
+		return generatePrivateKeyPKCS1({
+			format: "pem",
+			...options
+		});
+	}
 };
