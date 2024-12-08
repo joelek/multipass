972610083accaff19034936d57401972b0b331a3
Joel Ek <contact@joelek.se>
2024-12-08 16:01:47 +0100

    1.2.4

--- dist/cli/index.bundle.js
+++ dist/cli/index.bundle.js
@@ -15,8 +15,8 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 define("build/app", [], {
     "name": "@joelek/multipass",
-    "timestamp": 1733670065391,
-    "version": "1.2.3"
+    "timestamp": 1733670093988,
+    "version": "1.2.4"
 });
 define("node_modules/@joelek/autoguard/dist/lib-shared/serialization", ["require", "exports"], function (require, exports) {
     "use strict";
